882bd9f85d10f53ddfdc7e8c58be820e410538fa
Seth0r <github@seth0r.net>
2020-05-02 14:54:06 +0200

    fixed database initialization

--- dbinit.sh
+++ dbinit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo -n "Please enter the password for the MySQL root user: "
-read ROOT_PASS
-mysql --host "MYSQL_DB_HOST" -u root -p"$ROOT_PASS" <<< "CREATE DATABASE IF NOT EXISTS `$MYSQL_DB_NAME`"
-envsubst < /var/www/logger/doc/user.sql | mysql --host "MYSQL_DB_HOST" -u root -p"$ROOT_PASS"
-mysql --host "MYSQL_DB_HOST" -u root -p"$ROOT_PASS" "$MYSQL_DB_NAME" < /var/www/logger/doc/db.sql
+read -s ROOT_PASS
+echo
+mysql --host "$MYSQL_DB_HOST" -u root -p"$ROOT_PASS" <<< "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DB_NAME\`"
+
+grep -v "^CREATE DATABASE" /var/www/logger/doc/db.sql | grep -v "^USE" | mysql --host "$MYSQL_DB_HOST" -u root -p"$ROOT_PASS" "$MYSQL_DB_NAME"
+
+envsubst < /var/www/logger/doc/user.sql | mysql --host "$MYSQL_DB_HOST" -u root -p"$ROOT_PASS"
