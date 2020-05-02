66816569aa2b66639755520318dc69a58c618509
Seth0r <github@seth0r.net>
2020-05-02 15:53:09 +0200

    using the same schema for env vars

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -n "$LGGR_USER" -a -n "$LGGR_PASSWORD" ]; then
-    htpasswd -b -c /var/www/webuser "$LGGR_USER" "$LGGR_PASSWORD"
+if [ -n "$LGGR_USER" -a -n "$LGGR_PASS" ]; then
+    htpasswd -b -c /var/www/webuser "$LGGR_USER" "$LGGR_PASS"
 fi
 envsubst < /etc/php/7.3/apache2/php.ini.prep > /etc/php/7.3/apache2/php.ini 
 envsubst < /etc/php/7.3/cli/php.ini.prep > /etc/php/7.3/cli/php.ini 
